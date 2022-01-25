d99878037f4988e384d5873c801c25605b68c42d
jb <jebi0825@naver.com>
2022-01-25 18:08:31 +0900

    commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+let datas = [
+  {
+    no: 1,
+    name: "name1",
+    phone: "010-1111-2222",
+  },
+  {
+    no: 2,
+    name: "name2",
+    phone: "010-1111-2222",
+  },
+  {
+    no: 3,
+    name: "name3",
+    phone: "010-1111-2222",
+  },
+  {
+    no: 4,
+    name: "name4",
+    phone: "010-1111-2222",
+  },
+  {
+    no: 5,
+    name: "name5",
+    phone: "010-1111-2222",
+  },
+  {
+    no: 6,
+    name: "name6",
+    phone: "010-1111-2222",
+  },
+];
+
+export function getDatas() {
+  return datas;
+}
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import { getDatas } from "data";
 
 export default function About() {
   let params = useParams();
+  let datas = getDatas();
+
+  let result = datas
+    .filter(i => i.name === params.name)
+    .map((i, idx) => {
+      return (
+        <div key={idx}>
+          <p>no: {i.no}</p>
+          <p>name: {i.name}</p>
+          <p>phone: {i.phone}</p>
+        </div>
+      );
+    });
 
   return (
     <div>
       <h2>About {params.name}</h2>
+      {result.length === 0 ? <p>No Search Users!</p> : result}
     </div>
   );
 }
